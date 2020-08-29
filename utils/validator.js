ce0890a3e3acb5d91e14e4e845c100602e318ade
Raphael Etim <raphaelumoh@gmail.com>
2020-08-29 03:06:26 +0100

    implement dateString validator

---
+++ utils/validator.js
@@ -0,0 +1,17 @@
+
+const isString = (value) => {
+  return !!value && (typeof value === "string" || value instanceof String);
+};
+
+const dateStringValidator = (value, param) => ({
+  valid:
+    !!value &&
+    isString(value) &&
+    /\d{4}-\d{2}-\d{2}/.test(value) &&
+    !isNaN(new Date(value)),
+  message: param.name + ' must be in "YYYY-MM-DD" format',
+});
+
+module.exports = {
+  dateStringValidator
+}

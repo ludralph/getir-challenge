e1707bcd66cff01a01af66def62e2cdc66f3a1bb
Raphael Etim <raphaelumoh@gmail.com>
2020-08-29 08:09:11 +0100

    add unit test for response

---
+++ __tests__/utils/response.test.js
@@ -0,0 +1,72 @@
+const response = require('../../utils/response');
+
+let res;
+
+beforeEach(() => {
+  res = {
+    status: jest.fn(() => res),
+    json: jest.fn(() => res),
+    end: jest.fn(() => res),
+  };
+});
+
+describe("success", () => {
+  it("responds with success code, message and object", () => {
+    expect(response.success(res)({ prop: "value" })).toBeUndefined();
+    expect(res.status).toBeCalledWith(200);
+    expect(res.json).toBeCalledWith({ code: 0, msg: "Success", prop: "value" });
+  });
+});
+
+describe("clientErrorHandler", () => {
+  it("responds with 404 when there is an error on bodymen", () => {
+    const clientErrorHandler = response.clientErrorHandler();
+    const error = { message: "bodymen error" };
+    expect(clientErrorHandler({}, { bodymen: { error } }, res)).toBeUndefined();
+    expect(res.status).toBeCalledWith(400);
+    expect(res.json).toBeCalledWith({
+      code: 1,
+      msg: "bodymen error",
+      error,
+    });
+  });
+});
+
+describe("serverErrorHandler", () => {
+  it("responds with status 500 and error msg when error occurred on something", () => {
+    const serverErrorHandler = response.serverErrorHandler();
+    const error = { msg: "some msg", details: "some details" };
+    expect(serverErrorHandler(error, {}, res)).toBeUndefined();
+    expect(res.status).toBeCalledWith(500);
+    expect(res.json).toBeCalledWith({ code: 2, msg: "some msg", error });
+  });
+
+  it("responds with status 500 and error message when error occurred on something", () => {
+    const serverErrorHandler = response.serverErrorHandler();
+    const error = { message: "some message", details: "some details" };
+    expect(serverErrorHandler(error, {}, res)).toBeUndefined();
+    expect(res.status).toBeCalledWith(500);
+    expect(res.json).toBeCalledWith({ code: 2, msg: "some message", error });
+  });
+
+  it("responds with status 500 and default message when error occurred on something and there is no error message", () => {
+    const serverErrorHandler = response.serverErrorHandler();
+    const error = { details: "some details" };
+    expect(serverErrorHandler(error, {}, res)).toBeUndefined();
+    expect(res.status).toBeCalledWith(500);
+    expect(res.json).toBeCalledWith({
+      code: 2,
+      msg: "Unknown error occurred",
+      error,
+    });
+  });
+});
+
+describe("notFoundHandler", () => {
+  it("responds with status 404 when unknown route", () => {
+    const notFoundHandler = response.notFoundHandler();
+    expect(notFoundHandler({}, res)).toBeUndefined();
+    expect(res.status).toBeCalledWith(404);
+    expect(res.json).toBeCalledWith({ code: 3, msg: "Not found" });
+  });
+});
